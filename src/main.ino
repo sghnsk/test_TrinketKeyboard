ea796c80d6cfb91643c349778068185543070119
sghnsk <sgh.nsk@gmail.com>
2021-02-16 20:41:30 +0700

    test TrinketKeyboard

--- src/main.ino
+++ src/main.ino
@@ -6,43 +6,42 @@ For Trinket by Adafruit Industries
 
 #include <TrinketKeyboard.h>
 
-#define PIN_BUTTON_CAPITAL_A 0
-#define PIN_BUTTON_STRING    2
+#define LED_PIN 1
+#define KEY_CAPS_LOCK 0x39
 
-void setup()
-{
-  // button pins as inputs
-  pinMode(PIN_BUTTON_CAPITAL_A, INPUT);
-  pinMode(PIN_BUTTON_STRING, INPUT);
-
-  // setting input pins to high means turning on internal pull-up resistors
-  digitalWrite(PIN_BUTTON_CAPITAL_A, HIGH);
-  digitalWrite(PIN_BUTTON_STRING, HIGH);
-  // remember, the buttons are active-low, they read LOW when they are not pressed
-
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
   // start USB stuff
   TrinketKeyboard.begin();
 }
 
-void loop()
-{
+void loop() {
   TrinketKeyboard.poll();
   // the poll function must be called at least once every 10 ms
   // or cause a keystroke
   // if it is not, then the computer may think that the device
   // has stopped working, and give errors
 
-  if (digitalRead(PIN_BUTTON_CAPITAL_A) == LOW)
-  {
-    TrinketKeyboard.pressKey(KEYCODE_MOD_LEFT_SHIFT, KEYCODE_A);
-    // this should type a capital A
-    TrinketKeyboard.pressKey(0, 0);
-    // this releases the key
-  }
+  TrinketHidCompatibleDelay(1000);
 
-  if (digitalRead(PIN_BUTTON_STRING) == LOW)
-  {
-    // type out a string using the Print class
-    TrinketKeyboard.print("Hello World!");
+  uint8_t led_state = TrinketKeyboard.getLEDstate();
+
+  //test prints
+  uint8_t buttonLEDstate = TrinketKeyboard.getLEDstate();
+  TrinketKeyboard.print(buttonLEDstate,BIN); TrinketKeyboard.print(", "); TrinketKeyboard.println(buttonLEDstate);
+  TrinketKeyboard.print("test"); TrinketKeyboard.print(", "); TrinketKeyboard.println(buttonLEDstate);
+  TrinketKeyboard.println();
+
+  //light up onboard LED when Caps Lock is on
+  digitalWrite(LED_PIN, (TrinketKeyboard.getLEDstate()&KB_LED_CAPS)?HIGH:LOW);  
+
+
+}
+
+//this is a USB-compatible delay, so that the 10ms max time between poll calls is not exceeded
+void TrinketHidCompatibleDelay(unsigned int desiredDelay) {
+  unsigned long t_start = millis(); //ms
+  while (millis()-t_start<desiredDelay) {
+    TrinketKeyboard.poll();
   }
 }
