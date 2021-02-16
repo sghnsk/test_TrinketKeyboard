eeb55699fd799645678a669eeb52da6ac674d9f9
sghnsk <sgh.nsk@gmail.com>
2021-02-16 21:22:36 +0700

    test scroll lock

--- src/main.ino
+++ src/main.ino
@@ -7,7 +7,6 @@ For Trinket by Adafruit Industries
 #include <TrinketKeyboard.h>
 
 #define LED_PIN 1
-#define KEY_CAPS_LOCK 0x39
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
@@ -16,32 +15,12 @@ void setup() {
 }
 
 void loop() {
+  delay(5);
   TrinketKeyboard.poll();
-  // the poll function must be called at least once every 10 ms
-  // or cause a keystroke
-  // if it is not, then the computer may think that the device
-  // has stopped working, and give errors
-
-  TrinketHidCompatibleDelay(1000);
-
-  uint8_t led_state = TrinketKeyboard.getLEDstate();
-
-  //test prints
-  uint8_t buttonLEDstate = TrinketKeyboard.getLEDstate();
-  TrinketKeyboard.print(buttonLEDstate,BIN); TrinketKeyboard.print(", "); TrinketKeyboard.println(buttonLEDstate);
-  TrinketKeyboard.print("test"); TrinketKeyboard.print(", "); TrinketKeyboard.println(buttonLEDstate);
-  TrinketKeyboard.println();
-
+  
   //light up onboard LED when Caps Lock is on
-  digitalWrite(LED_PIN, (TrinketKeyboard.getLEDstate()&KB_LED_CAPS)?HIGH:LOW);  
+  digitalWrite(LED_PIN, (TrinketKeyboard.getLEDstate()&KB_LED_SCROLL)?HIGH:LOW);  
 
 
 }
 
-//this is a USB-compatible delay, so that the 10ms max time between poll calls is not exceeded
-void TrinketHidCompatibleDelay(unsigned int desiredDelay) {
-  unsigned long t_start = millis(); //ms
-  while (millis()-t_start<desiredDelay) {
-    TrinketKeyboard.poll();
-  }
-}
