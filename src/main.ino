dfbb12710b92488516ce47e562ff0b87967a4fa5
sghnsk <sgh.nsk@gmail.com>
2021-02-18 22:11:12 +0700

    Пример с обработкой нескольких нажатий scrolllock
    - время ожидания на 2 нажатия (KB_LED_STATE_NUM) scrolllock (KB_LED_STATE_MASK) задал опытным путем 900 ms (KB_LED_STATE_WAIT)
    - в случает срабатывания дергает D1 на 200 ms (LED_PIN_DELAY)

---
+++ src/main.ino
@@ -0,0 +1,39 @@
+#include <Arduino.h>
+#include <TrinketKeyboard.h>
+
+#define LED_PIN 1
+#define LED_PIN_DELAY 200
+#define KB_LED_STATE_MASK KB_LED_SCROLL
+#define KB_LED_STATE_WAIT 900
+#define KB_LED_STATE_NUM 2
+
+unsigned long previousMillis = 0;
+uint8_t lastKbLedState = 0;
+int countKbLedState = 0;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  TrinketKeyboard.begin();
+}
+
+void loop() {
+  delay(5);
+  TrinketKeyboard.poll();
+
+  uint8_t kbledState = TrinketKeyboard.getLEDstate() & KB_LED_STATE_MASK;
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= KB_LED_STATE_WAIT) {
+    previousMillis = currentMillis;
+    countKbLedState = 0;
+  } else {
+    if (kbledState != lastKbLedState)
+        countKbLedState++;
+  }
+  lastKbLedState = kbledState;
+  if (countKbLedState == KB_LED_STATE_NUM) {
+    countKbLedState = 0;
+    digitalWrite(LED_PIN, HIGH);
+    delay(LED_PIN_DELAY);
+    digitalWrite(LED_PIN, LOW);
+  }
+}
